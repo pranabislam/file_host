8afe731153bef46d03eed1cb98a6c7dccb1ff604
Pranab Islam <islampranab@gmail.com>
2023-11-28 12:29:18 -0500

    Create another_module.py

---
+++ another_module.py
@@ -0,0 +1,7 @@
+import logger_setup  # Import the logger
+
+def get_scores(task):
+    # Your get_scores functionality
+    # Now, you can use the global logger from logger_setup to log messages
+    logger_setup.logger.info(f"Logging from get_scores: Task {task} processing...")
+    # Other operations and logging here
